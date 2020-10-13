f99e84379c2f243dc75aafe2664f710131ab6c30
aldynoil <aldynomilham25@gmail.com>
2020-10-13 07:24:57 +0700

    Update Tugas 11

---
+++ src/Tugas-11/tugas11.js
@@ -0,0 +1,64 @@
+import React, {Component} from 'react'
+
+class Timer extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      time: 4,
+      showTime: true,
+      date: new Date()
+    }
+  }
+
+//   Date
+  currentTime(){
+    this.setState({
+        date: new Date(),
+    })
+}
+
+componentDidMount(){
+    setInterval(() => this.currentTime(), 1000)
+    this.myInterval = setInterval(() =>{
+        this.setState(({time}) => ({
+            time: time -1
+        }))
+    }, 1000)
+}
+
+
+  hideTime(){
+      this.setState({showTime: false})
+  }
+
+  componentWillUpdate(){
+    if(this.state.time === 0){
+        this.state.showTime=false
+        this.hideTime()
+
+    }
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.timerID);
+  }
+
+//   tick() {
+//     this.setState({
+//       time: this.state.time + 1 
+//     });
+//   }
+
+
+  render(){
+    return(
+      <>
+        <h1 style={{textAlign: "center"}}>
+        Sekarang Jam: {this.state.date.toLocaleTimeString("id-ID",{hour12:true})} / Hitung Mundur: {this.state.time}
+        </h1>
+      </>
+    )
+  }
+}
+
+export default Timer
